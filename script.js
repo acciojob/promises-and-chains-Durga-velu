d3b297afb7c5d9155a7cd43d39af2a0f0ad485e1
Durga-velu <durgadevi134babu@gmail.com>
2025-04-09 00:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let Fname=document.getElementById("name")
+let age=document.getElementById("age")
+
+let promise =new promise((resolve,reject)=>{
+
+	setTimeout(()=>{
+		resolve(if( Fname.value !== "" && age.value !== ""){
+			 if(age>=18){
+				 `Welcome ${Fname.vale}, You can vote`
+			 }
+			else{
+				`Oh sorry ${Fname.value}. You aren't old enough`
+			}
+		}
+		else{
+			`Please enter valid details.`
+		})
+	})
+},4000)
+
+promise.then((resolve)=>{
+
+	alert(resolve)
+}).catch((reject)=>{
+	alert reject
+})
